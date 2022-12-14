f863b026f6d4a642261827a02d23595507c7e45a
devauxjerome <90784691+devauxjerome@users.noreply.github.com>
2022-12-14 17:55:47 +0100

    get profil coach begining

--- src/components/MyCoach.vue
+++ src/components/MyCoach.vue
@@ -1,4 +1,5 @@
 <template>
+  
   <v-container class="Profile-page pa-0 ma-0" v-if="!edit">
     <v-img class="grey backImage" contain src="../assets/dunking.png"></v-img>
     <v-col>
@@ -162,117 +163,7 @@
       <v-btn type="submit"> Validate changes </v-btn>
     </v-form>
   </v-container>
-  <!-- <v-card class="mx-auto" max-width="434" tile>
-          <v-img height="100%" src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"></v-img>
-          <v-col>
-            <v-avatar size="100" style="position:absolute; top: 130px">
-              <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
-            </v-avatar>
-          </v-col>
-            <v-list-item color="rgba(0, 0, 0, .4)">
-              <v-list-item-content>
-                <v-list-item-title class="title">Marcus Obrien</v-list-item-title>
-                <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-      </v-card> -->
-  <!-- <v-row width="100%">
-          <v-card>
-            <v-img
-              src="https://picsum.photos/350/165?random"
-              height="125"
-
-              class="grey darken-4"
-            ></v-img>
-            <v-card-title class="text-h6">
-              height
-            </v-card-title>
-          </v-card>
-        </v-row>
-
-        <v-row>
-          <v-card width="100%">
-            <v-img
-              src="https://picsum.photos/350/165?random"
-              height="125"
-              width="100%"
-              contain
-              class="grey darken-4"
-            ></v-img>
-            <v-card-title class="text-h6">
-              height with contain
-            </v-card-title>
-          </v-card>
-        </v-row> -->
 </template>
-
-<!-- <script>
-// import axios from 'axios';
-// import { server } from '../helper';
-import { mapGetters } from 'vuex'
-import { reactive} from 'vue';
-
-  export default {
-    name: "Profile",
-    // beforeMount() {
-    //   axios.get(server.baseURLDev+'users/myprofil', {
-    //       headers: {
-    //         'Authorize': localStorage.getItem('token')
-    //       }
-    //     })
-    //     .then((response) =>{
-    //       this.userINfo = response.data;
-    //       this.firstname = response.data.firstname;
-
-    //       console.log(this.firstname);
-    //       console.log("aa");
-    //     })
-    //     .catch(error => {
-    //         // handle error
-    //         console.log(error)
-    //       })
-
-    // },
-    created() {
-        this.$store.dispatch('searchUserByToken')
-        this.items[0].value = this.$store.getters.getFirstname
-    },
-    data () {
-        return reactive({
-        firstname: '',
-        headers: [
-          { text: "CATEGORY", value: "category" },
-          { text: "VALUE", value: "value"},
-        ],
-        items: [
-          { category: "Firstname", value: '' },
-          { category: "Lastname", value: "Dev"},
-          { category: "Email", value: "jerome@gmail.com"},
-          { category: "Age", value: "22"},
-          { category: "Role", value: "Player"},
-          { category: "Sport", value: "Football"},
-          { category: "Position", value: "Forward"},
-          { category: "Size", value: "188cm"},
-          { category: "Weight", value: "78.2"},
-          { category: "Years of experience", value: "5"},
-          { category: "Actual CLub", value: "/"},
-          { category: "Adresse", value: "5030 Gembloux Belgium"},
-        ],
-
-    });},
-    computed: {
-        ...mapGetters({
-        getFFirstname: 'getFirstname',
-        }),
-    },
-
-    methods: {
-
-      
-    },
-    
-  }
-</script> -->
 <script>
 import axios from "axios";
 import { server } from "../helper";
@@ -287,11 +178,7 @@ export default {
 
   beforeMount() {
     axios
-      .get(server.baseURLDev + "users/myprofil", {
-        headers: {
-          Authorize: localStorage.getItem("token"),
-        },
-      })
+      .get(server.baseURLDev + "users/email/"+ localStorage.getItem("email"))
       .then((response) => {
         console.log(response);
         this.items[0].value = response.data.firstname;
@@ -303,16 +190,7 @@ export default {
         if (response.data.age != 0) {
           this.items[3].value = response.data.age;
         }
-        if (response.data.post.length != 0) {
-          this.items[6].value = response.data.post;
-        }
-        if (response.data.size != 0) {
-          this.items[7].value = response.data.size;
-        }
-        if (response.data.weight != 0) {
-          this.items[8].value = response.data.weight;
-        }
-        this.items[9].value = response.data.number_year_experience;
+        this.items[6].value = response.data.number_year_experience;
         this.imageName = response.data.picture;
         this.imageUrl = `https://pfeimages.blob.core.windows.net/imagess/${this.imageName}`;
       })
@@ -376,13 +254,13 @@ export default {
         this.city = response.data[0].city;
         if (this.country != undefined) {
           if (this.city != undefined) {
-            this.items[11].value = this.country + " " + this.city;
+            this.items[8].value = this.country + " " + this.city;
           } else {
-            this.items[11].value = this.country;
+            this.items[8].value = this.country;
           }
         } else {
           if (this.city != undefined) {
-            this.items[11].value = this.city;
+            this.items[8].value = this.city;
           }
         }
       })
@@ -392,13 +270,13 @@ export default {
     axios
       .get(
         server.baseURLDev +
-          "users/userClub?email_user=" +
+          "coachs/coachClub?email_user=" +
           localStorage.getItem("email")
       )
       .then((response) => {
         console.log("Cluuuub");
         console.log(response);
-        this.items[10].value = response.data[0].name;
+        this.items[7].value = response.data[0].name;
       })
       .catch((error) => {
         console.log(error);
@@ -411,9 +289,8 @@ export default {
     imageData: null,
     country: "",
     city: "",
-    sports: ["Football", "Basketball", "Volley"],
-    description: "No description yet",
-    edit: false,
+    sports: [],
+    description: "",
     headers: [
       { text: "CATEGORY", value: "category" },
       { text: "VALUE", value: "value" },
@@ -425,149 +302,11 @@ export default {
       { category: "Age", value: "/" },
       { category: "Role", value: "/" },
       { category: "Sport", value: "/" },
-      { category: "Post", value: "/" },
-      { category: "Size", value: "/" },
-      { category: "Weight", value: "/" },
       { category: "Years of experience", value: "/" },
       { category: "Actual CLub", value: "/" },
       { category: "Adresse", value: "/" },
     ],
   }),
+}
 
-  methods: {
-    onFileChange(event) {
-      const file = event.target.files[0];
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        this.imageUrl = e.target.result;
-        this.imageData = file;
-      };
-      reader.readAsDataURL(file);
-    },
-    uploadImage() {
-      const formData = new FormData();
-      formData.append("image", this.imageData);
-
-      axios
-        .post(server.baseURLDev + "users/uploadImage", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Authorize: localStorage.getItem("token"),
-          },
-        })
-        .then((response) => {
-          // Handle the response from the server
-          console.log(response);
-        })
-        .catch((error) => {
-          // Handle any errors
-          console.log(error);
-        });
-    },
-    sportArrayToNameSportArray(array) {
-      var arraytoreturn = [];
-      array.forEach((element) => {
-        arraytoreturn.push(element.name);
-      });
-      return arraytoreturn;
-    },
-    handleEdit() {
-      this.edit = !this.edit;
-    },
-    handleUpdateProfil() {
-      console.log(localStorage.getItem("token"));
-      const headers = {
-        "Content-Type": "application/json",
-        Authorize: localStorage.getItem("token"),
-      };
-
-      const body = {
-        firstname: this.items[0].value,
-        lastname: this.items[1].value,
-        email: this.items[2].value,
-        age: this.items[3].value,
-        post: this.items[6].value,
-        size: this.items[7].value,
-        weight: this.items[8].value,
-        number_year_experience: this.items[9].value,
-        description: this.description,
-        picture: "",
-        
-      };
-
-      axios
-        .put(server.baseURLDev + "users/update", body, { headers: headers })
-        .then((response) => {
-          console.log("userUPDAAAATE");
-          console.log(response.data);
-        })
-        .catch((error) => {
-          // handle error
-          console.log("erroooooor");
-          console.log(error);
-        });
-
-      axios
-        .put(server.baseURLDev + "sports/addSport", {
-          name: this.items[5].value,
-          email: localStorage.getItem("email"),
-        })
-        .then((response) => {
-          console.log("add sport");
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      axios
-        .post(server.baseURLDev + "adresses/create", {
-          country: this.country,
-          city: this.city,
-          street: "",
-          number: 0,
-          email: localStorage.getItem("email"),
-        })
-        .then((response) => {
-          console.log("add adresse");
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      // location.reload()
-    },
-  },
-};
 </script>
-<style>
-.backImage {
-  height: 17%;
-  width: 100%;
-  background-color: gray;
-}
-
-.customize-table {
-  --easy-table-border: 1px solid #445269;
-  --easy-table-row-border: 1px solid #445269;
-  background: linear-gradient(#0e0d0d, #2e2e2e);
-  --easy-table-header-font-size: 14px;
-  --easy-table-header-height: 30px;
-  --easy-table-header-font-color: #c1cad4;
-  --easy-table-header-background-color: #2d3a4f;
-
-  --easy-table-body-even-row-font-color: #fff;
-  --easy-table-body-even-row-background-color: #000000;
-
-  --easy-table-body-row-font-color: #c0c7d2;
-  --easy-table-body-row-background-color: ;
-  --easy-table-body-row-height: 50px;
-  --easy-table-body-row-font-size: 14px;
-
-  --easy-table-body-row-hover-font-color: #2d3a4f;
-  --easy-table-body-row-hover-background-color: #eee;
-
-  --easy-table-body-item-padding: 10px 15px;
-}
-</style>
